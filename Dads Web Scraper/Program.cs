109f492aabbb8144fd29b68fb95844d5dcd85f45
Alana La Bouchardiere <alabouchardiere@geomatic.com.au>
2020-09-09 08:28:16 +1000

    finally got it working

--- Dads Web Scraper/Program.cs
+++ Dads Web Scraper/Program.cs
@@ -1,8 +1,16 @@
-﻿using Dads_Web_Scraper.Models;
+﻿using CsvHelper;
+
+using Dads_Web_Scraper.Models;
+
+using DocumentFormat.OpenXml.Drawing.Charts;
+
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Web.Helpers;
 
 namespace Dads_Web_Scraper
 {
@@ -11,31 +19,139 @@ namespace Dads_Web_Scraper
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
-            string url = "https://www.realcommercial.com.au/";
-            string searchString = "3138";
-            //Console.WriteLine("Hello World!");
+            string url = "https://www.realcommercial.com.au/for-sale/3000/";
+            string outputPath = "C:/Users/Alana/Desktop/Scraper.csv";
 
             driver.Navigate().GoToUrl(url);
-            driver.FindElement(By.XPath("//*[@id='wrapper']/div/div[3]/div[1]/div/form/div[1]/div/div[1]/input")).SendKeys(searchString);
-            driver.FindElement(By.XPath("//*[@id='wrapper']/div/div[3]/div[1]/div/form/div[1]/button")).Click();
             List<Property> propertyList = new List<Property>();
-            int i = 1;
-            System.Threading.Thread.Sleep(5000);
+            List<string> stringList = new List<string>();
+            stringList.Add($"Address|Size|Type|Price");
+            bool timeToEnd = false;
+            bool lastRound = false;
+
             do
             {
-                if (driver.FindElement(By.XPath($"//*[@id='wrapper']/div/div[3]/div/div[2]/div[1]/ol/li[{i}]/div/div[3]")).Displayed)
+                timeToEnd = lastRound;
+                int propertyNumber = 1;
+
+                IList<IWebElement> propertiesOnPage = driver.FindElements(By.XPath("//*[@id='wrapper']/div/div[3]/div[1]/div[2]/div[1]/ol/li"));
+                if (propertiesOnPage.Count == 0)
                 {
-                    Property property = new Property()
-                    {
-                        Address = driver.FindElement(By.XPath($"//*[@id='wrapper']/div/div[3]/div/div[2]/div[1]/ol/li[{i}]/div/div[3]/div[1]/h2/a")).GetAttribute("title"),
-                        Size = driver.FindElement(By.XPath($"//*[@id='wrapper']/div/div[3]/div/div[2]/div[1]/ol/li[{i}]/div/div[3]/div[1]/div/span[1]")).Text,
-                        Type = driver.FindElement(By.XPath($"//*[@id='wrapper']/div/div[3]/div/div[2]/div[1]/ol/li[{i}]/div/div[3]/div[1]/div/span[2]")).Text,
-                        Price = driver.FindElement(By.XPath($"//*[@id='wrapper']/div/div[3]/div/div[2]/div[1]/ol/li[{i}]/div/div[3]/div[1]/h3")).Text
-                    };
-                    propertyList.Add(property);
+                    break;
                 }
-                i++;
-            } while (driver.FindElement(By.XPath($"//*[@id='wrapper']/div/div[3]/div/div[2]/div[1]/ol/li[{i}]")).Displayed);
+
+                foreach (IWebElement propertyOnPage in propertiesOnPage)
+                {
+
+                    string xPathPreString = $"//*[@id='wrapper']/div/div[3]/div[1]/div[2]/div[1]/ol/li[{propertyNumber}]";
+                    string xpathPropertyVariation1 = "/div/div[1]/div[2]/div[1]/h2/a";
+                    string xpathPropertyVariation2 = "/div/div[3]/div[1]/h2/a";
+                    string xpathPropertyVariation3 = "/div/div[2]/div[2]/div/h2/a";
+
+                    string address = "";
+                    string price = "";
+                    string size = "";
+                    string type = "";
+                    bool addProperty = false;
+
+
+
+                    if (driver.FindElements(By.XPath(xPathPreString + "/div/div")).Count == 4) //big listing
+                    {
+                        address = driver.FindElement(By.XPath(xPathPreString + xpathPropertyVariation2)).GetAttribute("title");
+                        price = driver.FindElement(By.XPath(xPathPreString + "/div/div[3]/div[1]/h3")).Text;
+                        size = driver.FindElement(By.XPath(xPathPreString + "/div/div[3]/div[1]/div/span[1]")).Text;
+
+                        if (driver.FindElements(By.XPath(xPathPreString + "/div/div[3]/div[1]/div/span")).Count == 2)
+                        {
+                            type = driver.FindElement(By.XPath(xPathPreString + "/div/div[3]/div[1]/div/span[2]")).Text;
+                        }
+                        else
+                        {
+                            type = driver.FindElement(By.XPath(xPathPreString + "/div/div[3]/div[1]/div/span[3]")).Text;
+                        }
+
+                        addProperty = true;
+                    }
+                    else if (driver.FindElements(By.XPath(xPathPreString + "/div/div")).Count == 2)
+                    {
+
+                        if (driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div")).Count == 1)//small listing end
+                        {
+                            address = driver.FindElement(By.XPath(xPathPreString + xpathPropertyVariation3)).GetAttribute("title");
+                            price = driver.FindElement(By.XPath(xPathPreString + "/div/div[2]/div[2]/div/h3")).Text;
+                            size = driver.FindElement(By.XPath(xPathPreString + "/div/div[2]/div[2]/div/div/span[1]")).Text;
+                            type = driver.FindElement(By.XPath(xPathPreString + "/div/div[2]/div[2]/div/div/span[2]")).Text;
+                            addProperty = true;
+                        }
+                        else if (driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div")).Count == 2)//small listing start
+                        {
+
+                            int asd = driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div[2]/div")).Count;
+                            int asasdd = driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div[2]/h2")).Count;
+                            if (driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div[2]/div")).Count == 2)
+                            {
+                                address = driver.FindElement(By.XPath(xPathPreString + xpathPropertyVariation1)).GetAttribute("title");
+                            }
+                            else
+                            {
+                                address = driver.FindElement(By.XPath(xPathPreString + "/div/div[1]/div[2]/h2/a")).GetAttribute("title");
+                            }
+
+                            price = driver.FindElement(By.XPath(xPathPreString + "/div/div[1]/div[2]/h3")).Text;
+
+                            if (driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div[2]/div")).Count == 2)
+                            {
+                                size = driver.FindElement(By.XPath(xPathPreString + "/div/div[1]/div[2]/div[2]/span[1]")).Text;
+                            } else
+                            {
+                                size = driver.FindElement(By.XPath(xPathPreString + "/div/div[1]/div[2]/div/span[1]")).Text;
+                            }
+
+                            if (driver.FindElements(By.XPath(xPathPreString + "/div/div[1]/div[2]/div[2]/span")).Count == 3)
+                            {
+                                type = driver.FindElement(By.XPath(xPathPreString + "/div/div[1]/div[2]/div[2]/span[3]")).Text;
+                            }
+                            else
+                            {
+                                type = driver.FindElement(By.XPath(xPathPreString + "/div/div[1]/div[2]/div/span[1]")).Text;
+                            }
+
+                            addProperty = true;
+                        }
+                    }
+
+                    if (addProperty)
+                    {
+                        Property property = new Property()
+                        {
+                            Address = address,
+                            Price = price,
+                            Size = size,
+                            Type = type
+
+                        };
+                        propertyList.Add(property);
+                        stringList.Add($"{property.Address}|{property.Size}|{property.Type}|{property.Price}");
+                    }
+                    propertyNumber++;
+                }               
+
+                Thread.Sleep(600);
+                if (driver.FindElements(By.ClassName("NextPageButton_label_1CDrV")).Count > 0 && !timeToEnd)
+                {
+                    driver.FindElement(By.ClassName("NextPageButton_label_1CDrV")).Click();
+                }
+                Thread.Sleep(600);
+                if (driver.FindElements(By.ClassName("Pagination_notActive_3p6uc")).Count > 0)
+                {
+                    lastRound = true;
+                }
+
+            } while (!timeToEnd);
+
+            System.IO.File.WriteAllLines(outputPath, stringList);
+            driver.Quit();
         }
     }
 }
