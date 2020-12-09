1d81a103bb3ce894d47d6959c307618268078cee
Alana La Bouchardiere <alana.labouchardiere@ausnetservices.com.au>
2020-12-09 11:52:29 +1100

    modularized and added new website

--- Dads Web Scraper/Models/Property.cs
+++ Dads Web Scraper/Models/Property.cs
@@ -7,8 +7,30 @@ namespace Dads_Web_Scraper.Models
     public class Property
     {
         public string Address { get; set; }
+        public string Postcode { get; set; }
         public string Price { get; set; }
+        public string Zoning { get; set; }
+        public string LandArea { get; set; }
+        public string FloorArea { get; set; }
+        public string Url { get; set; }
+        public string CarSpaces { get; set; }
+        public string PropertyExtent { get; set; }
+        public string TenureType { get; set; }
+        public string Description { get; set; }
+        public string Agency { get; set; }
         public string Size { get; set; }
+        public string AgenctContactName { get; set; }
+        public string AgenctContactNumber { get; set; }
         public string Type { get; set; }
+        public string Link { get; set; }
+        public string ParkingInfo { get; set; }
+        public List<Attribute> Attributes { get; set; }
+
+    }
+
+    public class Attribute
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
     }
 }
---
+++ Dads Web Scraper/Processors/MainProcessor.cs
@@ -0,0 +1,44 @@
+﻿using Dads_Web_Scraper.Sources;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dads_Web_Scraper.Processors
+{
+    class MainProcessor
+    {
+        private readonly RealCommercial realCommercial;
+        private readonly CommercialRealEstate commercialRealEstate;
+
+        public MainProcessor()
+        {
+            this.realCommercial = new RealCommercial();
+            this.commercialRealEstate = new CommercialRealEstate();
+        }
+
+        public void Process(string outputLocation, string url)
+        {
+
+            IWebDriver driver = new ChromeDriver();       
+
+            driver.Navigate().GoToUrl(url);
+            List<string> stringList = new List<string>();
+           
+
+            if (url.Contains("realcommercial.com.au"))
+            {
+                stringList = realCommercial.GetPropertiesFromRealCommercial(driver);
+            }
+            else if (url.Contains("commercialrealestate.com.au"))
+            {
+                stringList = commercialRealEstate.GetPropertiesFromCommercialRealEstate(driver);
+            }
+
+            driver.Quit();
+            File.WriteAllLines(outputLocation, stringList);
+
+        }
+    }
+
+}
